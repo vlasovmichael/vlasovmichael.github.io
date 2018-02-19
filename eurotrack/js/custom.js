978fae5c02c327780c44ea285f5c48fe3425a5c8
Michael Vlasov <vlasovmichael88@gmail.com>
2018-02-19 18:05:56 +0200

    hey

---
+++ eurotrack/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // nav menu
+    $('.header-top__menu').on('click', function(){
+    	$(this).toggleClass('is-active');
+        $('.menu-nav').toggleClass('is-open');
+    });
+
+    
+
+});
