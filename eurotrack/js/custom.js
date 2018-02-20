9d408675fe3f6c1e05a1d171465a87716f049b98
Michael Vlasov <vlasovmichael88@gmail.com>
2018-02-20 18:41:25 +0200

    Felina media and desctop

--- eurotrack/js/custom.js
+++ eurotrack/js/custom.js
@@ -6,6 +6,26 @@ $(document).ready(function(){
         $('.menu-nav').toggleClass('is-open');
     });
 
-    
-
 });
+
+// yandex map
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [55.907228, 31.260503],
+            zoom: 12
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([55.907228, 31.260503], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "Содержимое <em>балуна</em> метки",
+            hintContent: "Хинт метки"
+        });
+
+    myMap.behaviors.disable('scrollZoom')
+    myMap.geoObjects.add(myPlacemark);
+
+}
