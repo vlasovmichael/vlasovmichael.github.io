08b06d355f2331d764ebc81d8829e1e8afe78169
Michael Vlasov <vlasovmichael88@gmail.com>
2018-04-15 20:03:46 +0300

    audio player

--- master911/js/custom.js
+++ master911/js/custom.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    // init audio player
+    $('audio').audioPlayer();
+
     // init select2
     $('.js-select2:not([multiple])').select2({
         width: '100%',
@@ -79,6 +82,7 @@ $(document).ready(function(){
         slidesToScroll: 3,
         autoplay: false,
         autoplaySpeed: 5000,
+        draggable: false,
         arrows: true
     });
 
@@ -93,103 +97,4 @@ $(document).ready(function(){
         arrows: false
     });
 
-});
-
-
-// 
-// Setup Canvas
-var canvas = document.getElementById('visualizer');
-var canvasContext = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight / 2;
-
-// Setup Audio Context
-var audioContext = new AudioContext();
-var audio = document.getElementById('audio');
-
-var source = audioContext.createMediaElementSource(audio);
-var analyser = audioContext.createAnalyser();
-
-source.connect(analyser);
-analyser.connect(audioContext.destination);
-
-var bufferLength = analyser.frequencyBinCount;
-var frequencyData = new Uint8Array(bufferLength);
-
-// Visualizer Settings
-function Visualizer() {
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  analyser.getByteFrequencyData(frequencyData);
-
-  var frequencyWidth = (canvas.width / bufferLength),
-    frequencyHeight = 0,
-    x = 0;
-
-  for (var increment = 0; increment < bufferLength; increment++) {
-    frequencyHeight = frequencyData[increment] * (canvas.height * 0.003);
-    
-  var my_gradient = canvasContext.createLinearGradient(0,0,canvas.width,0);
-  
-  my_gradient.addColorStop(0,"#FFFFFF");
-  my_gradient.addColorStop(1,"#FFFFFF");
-    
-  canvasContext.fillStyle = my_gradient;
-  canvasContext.fillRect(x, canvas.height - frequencyHeight, frequencyWidth, frequencyHeight);
-    x += frequencyWidth + 4;
-  }
-
-  call = requestAnimationFrame(Visualizer);
-}
-
-// Default Audio Variable
-var isPlaying = true;
-
-// Start Visualizer on Load
-var startVisualizer = function() {
-  isPlaying = !isPlaying;
-  Visualizer();
-}
-startVisualizer();
-
-// Audio and Visualizer Controls
-var controls = document.getElementById('controls');
-var ctrl_label = document.getElementById('control-label');
-
-controls.addEventListener('click', function() {
-  isPlaying = !isPlaying;
-
-  if (isPlaying) {
-    controls.innerHTML = "<span class='icon-volume-high'></span>";
-    controls.style.background = "#FFA500";
-    ctrl_label.innerHTML = "Audio <span>On</span>";
-    audio.play();
-    Visualizer();
-  } else {
-    controls.innerHTML = "<span class='icon-volume-mute''></span>";
-    controls.style.background = "#0000FF";
-    ctrl_label.innerHTML = "Audio <span>Off</span>";
-    audio.pause();
-    cancelAnimationFrame(call);
-    canvas.height = window.innerHeight / 2;
-  }
-});
-
-// Load the Audio
-// var request = new XMLHttpRequest();
-
-// request.open('GET', 'https://s3-us-west-2.amazonaws.com/harriscarney/audio/Foliation.mp3', true);
-// request.responseType = 'blob';
-
-// request.onload = function() {
-//   audio.src = window.URL.createObjectURL(request.response);
-//   console.log(request.response);
-// }
-
-// request.send();
-
-// Resize Canvas on Browser Resize
-window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight / 2;
 });
