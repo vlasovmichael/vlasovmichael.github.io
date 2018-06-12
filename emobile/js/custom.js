cba2861bf4083ab4f680d6bcc13328286e927b10
Michael Vlasov <vlasovmichael88@gmail.com>
2018-06-13 01:42:15 +0300

    emobile done

--- emobile/js/custom.js
+++ emobile/js/custom.js
@@ -1,10 +1,14 @@
-// init select2
-// $('.js-select2:not([multiple])').select2({
-//     width: '100%',
-//     minimumResultsForSearch: 30,
-//     placeholder: $(this).data('placeholder'),
-//     templateResult: formatState
-// });
+// modal
+$(".js-modal").fancybox({
+    beforeShow: function(){
+        $("body").css({'overflow-y':'hidden'});
+    },
+    afterClose: function(){
+        $("body").css({'overflow-y':'visible'});
+    },
+    autoFocus: false,
+    touch: false
+});
 
 // filter
 $('.js-filter-text').click(function(){
@@ -17,6 +21,9 @@ $('.js-filter-text').click(function(){
 
 $('.js-filter-head').click(function(){
     var content = $('.js-filter-content');
+    var el = $(this);
+
+    el.toggleClass('is-open');
     content.slideToggle();
 });
 
@@ -26,61 +33,88 @@ $('.js-filter-close').click(function(){
     content.slideUp();
 });
 
+// stars
+function setting_rating(rate, element) {
+    element.parent('.rating').find('a').each(function(j) {
+        var el = $(this);
+        if (++j <= rate) {
+            el.removeClass('unactivestar').addClass('activestar');
+        } else {
+            el.removeClass('activestar').addClass('unactivestar');
+        }
+    });
+}
 
-// header menu
-// $('.header__buter, .js-menu__close').click(function(){
-//     $('.header__buter').toggleClass('is-active');
-//     $('.header__menu').toggleClass('is-open');
-// })
+$('.rating a').on('mouseover', function() {
+    var el = $(this);
+    setting_rating(el.attr('data-attr'), el);
+});
+$('.rating a').on('mouseout', function() {
+    var el = $(this);
+    if (el.parent('.rating').find('a').hasClass('check'))
+        setting_rating(el.parent('.rating').find('a.check').attr('data-attr'), el);
+    else
+        setting_rating(0, el);
+});
+$('.rating a').on('click', function() {
+    var el = $(this);
+    el.addClass('check').siblings().removeClass('check');
+    $(this).parents('.item-raitng').find('input[name=rating]').val(el.attr('data-attr'));
+    el.parent('.rating').find('input[type=hidden]').val(el.attr('data-attr'));
+});
 
+// tabs
+$(function() {
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+});
 
-// $(document).on('click touchstart', function(event) {
-//     if (!$(event.target).closest('.header').length) {
-//         $('.header__buter').removeClass('is-active');
-//         $('.header__menu').removeClass('is-open');
-//     }
-// });
+// move filter data & function resize
+var move = function(more, less, size) {
+    $(window).outerWidth() < size ? $(more).children().appendTo(less) : $(less).children().appendTo(more);
+}
 
-// validation
-// $('.js-form-submit').on('click', function() {
-//     var el = $('.js-form-input');
-//     if($(el).val() == 0) {
-//         el.addClass('not-valid');
-//     }
-//     else{
-//         el.removeClass('not-valid');
-//     }
-// });
+var moveResize = function(more, less, size) {
+    var func = function() {
+        move(more, less, size);
+    }
+    func();
+    $(window).on('resize', function() {
+        func();
+    });
+}
 
-// reviews slider 
-// $('.js-reviews').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     autoplay: false,
-//     autoplaySpeed: 5000,
-//     draggable: false,
-//     arrows: true,
-//     responsive: [
-//     {
-//       breakpoint: 1281,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         arrows: true,
-//         adaptiveHeight: true
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         arrows: true,
-//         adaptiveHeight: true
-//       }
-//     }
-//   ]
-// });
+moveResize('.js-catalog-item-about-desktop', '.js-catalog-item-about-tablet', 1200);
+moveResize('.js-head-search-desktop', '.js-head-search-tablet', 1023);
+moveResize('.js-head-search-tablet', '.js-mobile-search', 767);
+moveResize('.js-nav', '.js-mobile-catalog', 767);
+moveResize('.js-head-top', '.js-mobile-menu', 767);
+moveResize('.js-desktop-phone', '.js-mobile-phone', 767);
+
+// tablet menu btn's
+$('.js-tablet-catalog-btn').on('click', function(){
+    $('.js-nav').slideToggle();
+});
+
+$('.js-tablet-menu-btn').on('click', function(){
+    $('.js-head-top').slideToggle();
+});
+
+// mobile menu btn's
+$('.js-mobile-btn').on('click', function(){
+    var item = $('.menu-mobile-list__item');
+    var content = $('.js-mobile-content');
+    var el = $(this);
+    var btn = $('.js-mobile-btn');
+
+    el.parents(item).find(btn).toggleClass('is-active');
+    el.closest(item).siblings().find(btn).removeClass('is-active');
+    $(this).next(content).slideToggle();
+    $(this).closest(item).siblings().find(content).slideUp();
+});
 
 // reviews slider 
 $('.js-main-slider').slick({
@@ -91,6 +125,53 @@ $('.js-main-slider').slick({
     draggable: false,
     arrows: true,
     dots: true
+});
+
+// sync catalog item carousel
+$('.js-catalog-item-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    infinite: true,
+    adaptiveHeight: true,
+    asNavFor: '.js-catalog-item-thumbs'
+});
+
+$('.js-catalog-item-thumbs').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.js-catalog-item-main',
+    dots: false,
+    arrows: true,
+    infinite: false,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 6
+            }
+        },
+        {
+            breakpoint: 570,
+            settings: {
+                slidesToShow: 5
+            }
+        },
+        {
+            breakpoint: 501,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 401,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+    ]
 });
 
 // range slider
